dcb99cc6f682b3d5dfa5d780e22f873be2f7be96
KostylevEugene <kostylev_evgenij@list.ru>
2020-12-03 05:23:12 +0500

    New commit

---
+++ webapp/__init__.py
@@ -0,0 +1,18 @@
+# __init__ назван так для того, чтобы при импорте проекта (webapp) в другой данный файл выполнится автоматически
+
+from flask import Flask, render_template
+from webapp.python_org_news import get_python_news
+from webapp.weather import weather_by_city
+
+def create_app():
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        title = "Новости Python"
+        weather = weather_by_city('Chelyabinsk')
+        news_list = get_python_news()
+        return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
+
+    return app
