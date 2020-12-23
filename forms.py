c20595535b98d610b74ce627476e02141229f2e1
KostylevEugene <kostylev_evgenij@list.ru>
2020-12-23 14:31:12 +0500

    authorization upd

--- forms.py
+++
@@ -1,6 +0,0 @@
-from flassk_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField         #импорт типов полей
-from wtforms.validators import DateRequired         # импорт валидатора - класс проверки данных
-
-class FlaskLogin(FlaskForm):
-    username = StringField('Имя пользователя', validate)
