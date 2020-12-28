a0bbc692fc7ad9cd80df74c1e5597743097d5f3c
KostylevEugene <kostylev_evgenij@list.ru>
2020-12-28 17:12:48 +0500

    added loguot route

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,7 @@
 # __init__ назван так для того, чтобы при импорте проекта (webapp) в другой данный файл выполнится автоматически
 
 from flask import Flask, render_template, flash, redirect, url_for
-from flask_login import LoginManager login_user 
+from flask_login import LoginManager, login_user, logout_user
 
 from webapp.forms import LoginForm
 from webapp.model import db, News
@@ -34,10 +34,9 @@ def create_app():
         login_form = LoginForm()
         return render_template('login.html', page_title=title, form=login_form)
 
-    @app.route('/process-login', method=['POST'])
+    @app.route('/process-login', methods=['POST'])
     def process_login():
         form = LoginForm()
-
         if form.validate_on_submit():
             user = User.query.filter(User.username == form.username.data).first()
             if user and user.check_password(form.password.data):
@@ -47,5 +46,10 @@ def create_app():
                     
         flash('Не правильное имя или пароль')
         return redirect(url_for('login'))
-        
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('Вы успешно вышли из учетной записи')
+        return redirect(url_for('index'))
+
     return app
