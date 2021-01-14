ea4b4e0d7ace6534c0079d69478a6ad4334bcbd6
KostylevEugene <kostylev_evgenij@list.ru>
2021-01-14 10:32:57 +0500

    fixed some bugs and updated requirements.txt

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,7 @@
 # __init__ назван так для того, чтобы при импорте проекта (webapp) в другой данный файл выполнится автоматически
 
-from flask import Flask, render_template, flash, redirect, url_for
-from flask_login import LoginManager, login_user, current_user, logout_user
+from flask import Flask, flash, render_template, redirect, url_for
+from flask_login import current_user, LoginManager, login_required, login_user, logout_user
 
 from webapp.forms import LoginForm
 from webapp.model import db, News, User
@@ -39,6 +39,7 @@ def create_app():
     @app.route('/process-login', methods=['POST'])
     def process_login():
         form = LoginForm()
+        
         if form.validate_on_submit():
             user = User.query.filter(User.username == form.username.data).first()
             if user and user.check_password(form.password.data):
@@ -54,5 +55,13 @@ def create_app():
         logout_user()
         flash('Вы успешно вышли из учетной записи')
         return redirect(url_for('index'))
+
+    @app.route('/admin')
+    @login_required
+    def admin_index():
+        if current_user.is_admin:
+            return "Привет Админ!"
+        else:
+            return "Ты не Админ!"
 
     return app
